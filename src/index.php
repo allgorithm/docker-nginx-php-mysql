5ad20e15d5fd2173bba133f35558117e9d5e6395
alexkrivonos <hrenotkroew>
2023-05-17 21:08:58 +0200

    add mysql, volume

--- src/index.php
+++ src/index.php
@@ -2,4 +2,12 @@
 
 declare(strict_types = 1);
 
-echo 'Hello world!';
+// echo 'Hello world!';
+
+$connection = mysqli_connect('mysql', 'root', 'root');
+
+$connection->query('CREATE DATABASE `mynp-docker`');
+
+// phpinfo();
+echo('<pre>');
+var_dump($connection);
